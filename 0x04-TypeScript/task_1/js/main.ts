3b91e92039d3ea87da13580e0cc85496134fb605
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-09-14 23:22:06 +0100

    Let's build a Teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,3 +21,28 @@ const printTeacher: printTeacherFunction = (
 ): string => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
