87e11a3a47ae6c3fbda7249d37a943465dae4162
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-09-14 23:33:57 +0100

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,24 +1,30 @@
-function printTeacher({
-    firstName,
-    lastName,
-}: {
+// Interface for the constructor
+interface StudentConstructor {
     firstName: string;
     lastName: string;
-}): string {
-    return `${firstName}. ${lastName}`;
-}
-interface printTeacherFunction {
-    (firstName: string, lastName: string): string;
-}
-interface Director extends Teacher {
-    numberOfReports: number;
 }
 
-interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: any; // Index signature to allow additional properties
+// Interface for the class methods
+interface StudentInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+// StudentClass implementation
+class StudentClass implements StudentInterface {
+    private firstName: string;
+    private lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
 }
