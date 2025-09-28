e76c033bb1a2634fdea977a580b3845f7de0e0e6
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-09-28 15:32:21 +0100

    class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -24,3 +24,30 @@ function printTeacher({
 }): string {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+class StudentClass implements StudentClassInterface {
+    private firstName: string;
+    private lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
