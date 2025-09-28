cab99a4cf2ebbacd0ef8a3caeffe5c8e3170c136
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-09-28 15:26:49 +0100

    task_3_fix

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,8 +15,12 @@ interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
-    return firstName.charAt(0) + ". " + lastName;
+function printTeacher({
+    firstName,
+    lastName,
+}: {
+    firstName: string;
+    lastName: string;
+}): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
 }
-
-console.log(printTeacher("John", "Doe")); // J. Doe}
