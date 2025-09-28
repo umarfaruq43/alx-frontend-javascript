ee3176720fd0d3e414a8a16014cf352fe78a174c
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-09-28 15:48:23 +0100

    task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,89 +1,53 @@
-// Define interfaces
-interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
 }
 
-interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
+interface Director extends Teacher {
+    numberOfReports: number;
 }
 
-// Implement classes
-class Director implements DirectorInterface {
-    workFromHome(): string {
-        return "Working from home";
-    }
-
-    getCoffeeBreak(): string {
-        return "Getting a coffee break";
-    }
-
-    workDirectorTasks(): string {
-        return "Getting to director tasks";
-    }
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
 }
 
-class Teacher implements TeacherInterface {
-    workFromHome(): string {
-        return "Cannot work from home";
-    }
-
-    getCoffeeBreak(): string {
-        return "Cannot have a break";
-    }
-
-    workTeacherTasks(): string {
-        return "Getting to work";
-    }
+function printTeacher({
+    firstName,
+    lastName,
+}: {
+    firstName: string;
+    lastName: string;
+}): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
 }
 
-// createEmployee function
-function createEmployee(salary: number | string): Director | Teacher {
-    if (typeof salary === "number" && salary < 500) {
-        return new Teacher();
-    } else {
-        return new Director();
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+class StudentClass implements StudentClassInterface {
+    private firstName: string;
+    private lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
     }
 }
-
-// Test cases
-console.log(createEmployee(200)); // Teacher
-console.log(createEmployee(1000)); // Director
-console.log(createEmployee("$500")); // Director
-
-// Type predicate function
-function isDirector(employee: Director | Teacher): employee is Director {
-    return employee instanceof Director;
-}
-
-// executeWork function
-function executeWork(employee: Director | Teacher): void {
-    if (isDirector(employee)) {
-        console.log(employee.workDirectorTasks());
-    } else {
-        console.log(employee.workTeacherTasks());
-    }
-}
-
-// Test cases
-executeWork(createEmployee(200)); // Getting to work
-executeWork(createEmployee(1000)); // Getting to director tasks
-
-// String literal type
-type Subjects = "Math" | "History";
-
-// teachClass function
-function teachClass(todayClass: Subjects): string {
-    if (todayClass === "Math") {
-        return "Teaching Math";
-    } else {
-        return "Teaching History";
-    }
-}
-
-// Test cases
-console.log(teachClass("Math")); // Teaching Math
-console.log(teachClass("History")); // Teaching History
