014128af6e971981ba894bc2dd2f696fe1e8d326
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-09-28 15:08:32 +0100

    task_3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,11 +10,18 @@ interface Teacher {
 interface Director extends Teacher {
     numberOfReports: number;
 }
-
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
-    return `${firstName}. ${lastName}`;
+function printTeacher({
+    firstName,
+    lastName,
+}: {
+    firstName: string;
+    lastName: string;
+}): string {
+    return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
