9e191cb69db8c93794d95846fd1ba37b9628d353
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2025-09-28 15:11:44 +0100

    task_3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,10 +4,11 @@ interface Teacher {
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
-    [key: string]: any;
+    contract: boolean;
+    [propName: string]: any;
 }
 
-interface Director extends Teacher {
+interface Directors extends Teacher {
     numberOfReports: number;
 }
 
@@ -18,3 +19,32 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName[0]}. ${lastName}`;
 };
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+const StudentClass: StudentConstructor = class StudentClass
+    implements StudentClassInterface
+{
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return "Currently working";
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+};
+
+export { printTeacher, StudentClass };
